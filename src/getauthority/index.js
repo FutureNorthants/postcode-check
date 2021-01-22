5f1fde9df7dacd2115e19901b6e2fc00f417235b
willwise <wwise@northamptonshire.gov.uk>
2021-01-22 10:28:18 +0000

    adding codes for unitary and sovereign council

--- src/getauthority/index.js
+++ src/getauthority/index.js
@@ -34,89 +34,132 @@ async function getBasicCouncilByPostcode(event) {
 
     var oldAuth;
     var newAuth;
+    var sovereignCode;
+    var unitaryCode;
 
     switch(postcodeStart){
         case "NN1":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN2":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN3":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN4":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN5":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN6":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN7":
             oldAuth = 'Northampton';
             newAuth = 'West';
+            sovereignCode = 5;
+            unitaryCode = 2;
             break;
         case "NN8":
             oldAuth = 'Wellingborough';
             newAuth = 'North';
+            sovereignCode = 7;
+            unitaryCode = 1;
             break;
         case "NN9":
             oldAuth = 'Wellingborough';
             newAuth = 'North';
+            sovereignCode = 7;
+            unitaryCode = 1;
             break;
         case "NN29":
             oldAuth = 'Wellingborough';
             newAuth = 'North';
+            sovereignCode = 7;
+            unitaryCode = 1;
             break;
         case "NN10":
             oldAuth = 'East Northants';
             newAuth = 'North';
+            sovereignCode = 3;
+            unitaryCode = 1;
             break;
         case "NN11":
             oldAuth = 'Daventry';
             newAuth = 'West';
+            sovereignCode = 2;
+            unitaryCode = 2;
             break;
         case "NN12":
             oldAuth = 'South Northants';
             newAuth = 'West';
+            sovereignCode = 6;
+            unitaryCode = 2;
             break;
         case "NN13":
             oldAuth = 'South Northants';
             newAuth = 'West';
+            sovereignCode = 6;
+            unitaryCode = 2;
             break;
         case "NN14":
             oldAuth = 'Kettering';
             newAuth = 'North';
+            sovereignCode = 4;
+            unitaryCode = 1;
             break;
         case "NN15":
             oldAuth = 'Kettering';
             newAuth = 'North';
+            sovereignCode = 4;
+            unitaryCode = 1;
             break;
         case "NN16":
             oldAuth = 'Kettering';
             newAuth = 'North';
+            sovereignCode = 4;
+            unitaryCode = 1;
             break;
         case "NN17":
             oldAuth = 'Corby';
             newAuth = 'North';
+            sovereignCode = 1;
+            unitaryCode = 1;
             break;        
         case "NN18":
             oldAuth = 'Corby';
             newAuth = 'North';
+            sovereignCode = 1;
+            unitaryCode = 1;
             break;
     }
     
     var body = {            
             unitary: newAuth,
-            sovereign: oldAuth
+            unitaryCode: unitaryCode,
+            sovereign: oldAuth,
+            sovereignCode: sovereignCode
+
     };
     return {
         statusCode: 200,
